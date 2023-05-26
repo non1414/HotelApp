3d9345178aaee6096efffebda728985e665969a6
non1414 <nouf22228@gmail.com>
2023-05-26 16:52:44 +0300

    Initial Commit

---
+++ HotelApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HotelApp
+//
+//  Created by نوف بخيت الغامدي on 06/11/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotelApp/HotelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelAppApp.swift
+//  HotelApp
+//
+//  Created by نوف بخيت الغامدي on 06/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct HotelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
